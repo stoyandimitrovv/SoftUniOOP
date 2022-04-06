94a0fc670daf6a36fd9b09fec3f5b9816511b57f
Stoyan <stoqnd1999@gmail.com>
2022-04-06 13:40:14 +0300

    OOP

---
+++ OOP/ReflectionAndAttributes/ReflectionAndAttributes/Core/CommandInterpreter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] input = args
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = input[0] + "Command";
+
+            string[] value = input.Skip(1).ToArray();
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(x => x.Name == commandName);
+
+            if (type == null)
+            {
+                throw new InvalidOperationException("Invalid command");
+            }
+
+            ICommand command = Activator.CreateInstance(type) as ICommand;
+
+            string result = command.Execute(value);
+
+            return result;
+        }
+    }
+}
---
+++ OOP/ReflectionAndAttributes/ReflectionAndAttributes/Core/Engine.cs
@@ -0,0 +1,33 @@
+﻿namespace CommandPattern
+{
+    using System;
+
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                while (true)
+                {
+                    string args = Console.ReadLine();
+
+                    string result = this.commandInterpreter.Read(args);
+
+                    Console.WriteLine(result);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
